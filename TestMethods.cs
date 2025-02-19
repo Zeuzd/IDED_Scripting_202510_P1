8ad00b6c0c5ec9530e6109193e9690881484a8f4
Zeuzd <davidgalvis00@gmail.com>
2025-02-19 07:15:01 -0500

    Termine

--- TestMethods.cs
+++ TestMethods.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace TestProject1
 {
@@ -6,27 +7,107 @@ namespace TestProject1
     {
         internal static uint StackFirstPrime(Stack<uint> stack)
         {
-            return 0;
+            while (stack.Count > 0)
+            {
+                uint num = stack.Pop();
+                if (num < 2) continue;
+                if (num == 2 || num == 3) return num;
+                if (num % 2 == 0) continue;
+
+                bool isPrime = true;
+                for (uint i = 3; i * i <= num; i += 2)
+                {
+                    if (num % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime) return num;
+            }
+            return 0; 
         }
+
 
         internal static Stack<uint> RemoveFirstPrime(Stack<uint> stack)
         {
-            return null;
+            Stack<uint> tempStack = new Stack<uint>(); 
+            bool primeFound = false;
+
+            while (stack.Count > 0)
+            {
+                uint num = stack.Pop();
+
+                if (!primeFound && num >= 2)
+                {
+                    bool isPrime = true;
+                    if (num == 2 || num == 3) isPrime = true;
+                    else if (num % 2 == 0) isPrime = false;
+                    else
+                    {
+                        for (uint i = 3; i * i <= num; i += 2)
+                        {
+                            if (num % i == 0)
+                            {
+                                isPrime = false;
+                                break;
+                            }
+                        }
+                    }
+
+                    
+                    if (isPrime)
+                    {
+                        primeFound = true;
+                        continue;
+                    }
+                }
+
+                tempStack.Push(num);
+            }
+
+            while (tempStack.Count > 0)
+            {
+                stack.Push(tempStack.Pop());
+            }
+
+            return stack;
         }
+
 
         internal static Queue<uint> CreateQueueFromStack(Stack<uint> stack)
         {
-            return null;
+            return new Queue<uint>(stack);
         }
+
 
         internal static List<uint> StackToList(Stack<uint> stack)
         {
-            return null;
+            return new List<uint>(stack);
         }
+
 
         internal static bool FoundElementAfterSorted(List<int> list, int value)
         {
+
+            list.Sort();
+
+            foreach (int item in list)
+            {
+                if (item == value)
+                {
+                    return true;
+                }
+                
+                else if (item > value)
+                {
+                    return false;
+                }
+            }
+
             return false;
         }
+
     }
 }
